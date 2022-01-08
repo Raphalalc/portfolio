007d136951527f32dc3c907d007ce38285222d34
raphaelalc <85412668+Raphalalc@users.noreply.github.com>
2022-01-09 00:58:00 +0100

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,41 @@
+
+function openNav(){
+  document.getElementById("contact").style.visibility = "visible";
+
+  document.getElementById('contact').classList.remove('contactfermer');
+  document.getElementById('contact').classList.add('contactouvrir');
+}
+
+
+function closeNav(){
+  document.getElementById('contact').classList.remove('contactouvrir');
+  document.getElementById('contact').classList.add('contactfermer');
+
+}
+
+function openNav1(){
+  document.getElementById("contact1").style.visibility = "visible";
+
+  document.getElementById('contact1').classList.remove('contactfermer');
+  document.getElementById('contact1').classList.add('contactouvrir');
+}
+
+
+function closeNav1(){
+  document.getElementById('contact1').classList.remove('contactouvrir');
+  document.getElementById('contact1').classList.add('contactfermer');
+}
+
+function openNav2(){
+  document.getElementById("contact2").style.visibility = "visible";
+
+  document.getElementById('contact2').classList.remove('contactfermer');
+  document.getElementById('contact2').classList.add('contactouvrir');
+}
+
+
+function closeNav2(){
+  document.getElementById('contact2').classList.remove('contactouvrir');
+  document.getElementById('contact2').classList.add('contactfermer');
+
+}
